392c53ba009859c70c3e609a9717a65c024b9fcc
Hiroyuki Kondo <hombredeestemundo@gmail.com>
2016-06-27 14:05:51 +0900

    change websocket url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  WEBSOCKET_URL = "taxi-button.herokuapp.com/websocket"
+  WEBSOCKET_URL = "taxibutton.herokuapp.com/websocket"
 end
